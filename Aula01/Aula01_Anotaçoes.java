96f163902e746a724bf2db7a4899ebd597c2dd00
CaioRian187 <caiorian246810@gmail.com>
2025-08-26 22:47:11 -0300

    Spring Boot Project

---
+++ Aula01/Aula01_Anotaçoes.java
@@ -0,0 +1,35 @@
+package Aula01;
+/*                                          API RESTful com Spring Boot
+O curso é basicamente ensinar a como conectar o nosso banco de dados com o FrontEnd da nossa aplicação, ou seja, Transferencia de
+dados na nossa aplicação.
+
+SUMÁRIO:
+1 - Pré-Requisitos:
+    1 - Logica de programação
+    2 - Linguage java
+    3 - Conhecimentos basicos de metodos HTTP
+    4 - HTML e javasCript
+    5 - Banco de dados Relacional (SQL)
+    6 - Git e GitHub
+
+2 - O que são API RestFul?
+É basicamente receber uma requisição do cliente e manda para o banco de dados, posteriormente o banco de dados retorna o que fui
+solicitado (basicamente ligação entre o banco de dados e o cliente)
+RESTFul => É o termo para descrever APIs HTTP que adotam o padrao REST ; Pode-se dizer que é uma "ponte" entre o cliente (que pode
+ser o browser com a aplicação frontend ou até mesmo outra API/sistema) e o serviço do banco de dados.
+
+3 - O que é Spring Boot?
+É um frameWork que nasceu apartir do frameWork Spring ; Serve para facilitar o desenvolvimento de aplicações de pequeno e grande porte
+de forma independente ; Abstrai e facilita a configuração de serviços, genrenciamento de dependências, configurações de bibliotecas ;
+Maior parte das configurações pode ser feita de forma programavel via anotações ; Possui um novo modelo de desenvolvimento, mais simples
+e direto de que o do Spring FrameWork.
+ 
+4 - O que é MVC?
+MVC é um padrão arquitetural, O requerimento HTTP é feito pelo navegador, ai o request(Requerimento) é mandado para o controller, ai o
+controller faz a demanda para a model, ai o controller pega os dados da model, ai o controller envia os dados para a view, ai a 
+view vai mostrar para o usuario. 
+
+5 - Ferramentas e configurações de ambiente
+
+
+ */
