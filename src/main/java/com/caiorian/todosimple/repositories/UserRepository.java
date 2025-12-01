4a1a05d0eb48a132a7258cf99bdb6e37869ffad1
CaioRian187 <caiorian246810@gmail.com>
2025-12-01 13:16:59 -0300

    Create TaskRepository

--- src/main/java/com/caiorian/todosimple/repositories/UserRepository.java
+++ src/main/java/com/caiorian/todosimple/repositories/UserRepository.java
@@ -1,7 +1,6 @@
 package com.caiorian.todosimple.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 
 import com.caiorian.todosimple.models.User;
